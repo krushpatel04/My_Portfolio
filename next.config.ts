98dd941dcd4555843f7d259c27b83d61446b66e4
Krush Patel <krushp04@gmail.com>
2026-06-07 19:04:17 -0400

    bug fix, picture

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS === "true";
-
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/My_Portfolio",
+  assetPrefix: "/My_Portfolio",
   images: { unoptimized: true },
-  basePath: isGithubActions ? "/My_Portfolio" : "",
-  assetPrefix: isGithubActions ? "/My_Portfolio" : "",
 };
 
 export default nextConfig;
